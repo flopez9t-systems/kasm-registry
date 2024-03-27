1d9cf5fc656005e2d19dda72669b2b05e01e9a79
flopez9t-systems <150351022+flopez9t-systems@users.noreply.github.com>
2024-03-27 12:57:54 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Kasm Fran',
+    description: 'Test workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://flopez9.github.io/kasm-registry/',
+    contactUrl: https://github.com/flopez9t-systems/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
